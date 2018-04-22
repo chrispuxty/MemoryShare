0e618da869dc44ddd774e8b8726ec751387201d9
Liam Greenough <beacon515@gmail.com>
2018-04-22 18:29:29 +1000

    Config fixed.

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect($db,"127.0.0.1","kanvar","eVBLeUTKqRm8qxsQ") or die(mysqli_error($db));
+$db = mysqli_connect("127.0.0.1","kanvar","eVBLeUTKqRm8qxsQ") or die(mysqli_error($db));
 mysqli_select_db($db,"kanvar_dementia") or die(mysqli_error($db));
 $patientID = mysqli_fetch_assoc($db,mysqli_query($db,"SELECT id FROM options LIMIT 1;")) or die(mysqli_error($db));
 $patientID = $patientID['id'];
