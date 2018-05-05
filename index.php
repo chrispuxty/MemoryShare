45470b2641a18f841c741e19acd681998b18bafd
Liam Greenough <beacon515@gmail.com>
2018-05-06 00:34:11 +1000

    Bugfix.

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ for ($col = 0; $col < 4; $col++) if(strpos($path."/".$file,$media[$col])!==false
 //||!isset($_GET['recurse'])
 )?"<div class='blink' id='pauseButton' onclick='disableFullScreen();'></div><div class='blink' id='pauseButtonText'><img class='button-icon' src='Pause.png'><br/>Pause</div>":"")."
 <script type='text/javascript' src='".
-(($severeMode)?"js/fullscreenSevere.js":"js/fullscreen.js")."'></script>";
+(($severeMode)?"js/fullscreenSevere.js":"js/fullscreen.js")."'></script>"
 .(($severeMode
 ||true //HACK
 )?"
