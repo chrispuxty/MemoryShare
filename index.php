a800a83ca1bb9d438e7d8bf1b836bc2a7b30e088
Liam Greenough <beacon515@gmail.com>
2018-05-12 03:24:08 +1000

    Bugfix.

--- index.php
+++ index.php
@@ -334,7 +334,7 @@ echo '</div>';}
 if(strtolower($extension) == "kanvar") {echo "<div class='blink' id='pauseButton' onclick='disableFullScreen();'></div><div class='blink' id='pauseButtonText'  onclick='disableFullScreen();'><img class='button-icon' src='Pause.png'><br/>Pause</div>";}
 if ($file != "" || $path !="media") {echo '<div id="stopButtonText" onclick="window.open(\'index.php\',\'_self\');"><img class="button-icon" src="Stop.png"><br/>'.((true||$file != "")?'Stop':'Back').'</div>
 <div id="stopButton" onclick="window.open(\'index.php\',\'_self\');"></div>';
-echo '<div id="buttonDisabler"></div><div id="altPauseButton" onclick="'.(($severeMode)?"window.open('index.php','_self');":"toggleFullScreen();").'" style="'.(($file != ""&&$severeMode)?"display: block !important;":"display: block;").'">';
+echo '<div id="buttonDisabler"></div><div id="altPauseButton" onclick="'.(($severeMode)?"window.open('index.php','_self');":"toggleFullScreen();").'" style="'.(($file != ""&&$severeMode)?"display: block;":"").'">';
 if(!severe) for ($i = 0; $i < $mediaCount; $i++) echo '<div class="altButton"></div>';
 echo '</div>';}
 echo '<div id="pauseButtonReturner" onclick="window.open(\'index.php\',\'_self\');" style="'.(($file != "" && $severeMode && false
